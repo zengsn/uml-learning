b294f98ff2502338d4d4c674ab0938e0d32cc230
Zeng Shaoning <shaoning.zeng@gmail.com>
2011-07-25 23:02:18 +0800

    first commit

---
+++ sts/src/main/webapp/webtop.js
@@ -0,0 +1,144 @@
+/*
+
+This file is part of Ext JS 4
+
+Copyright (c) 2011 Sencha Inc
+
+Contact:  http://www.sencha.com/contact
+
+GNU General Public License Usage
+This file may be used under the terms of the GNU General Public License version 3.0 as published by the Free Software Foundation and appearing in the file LICENSE included in the packaging of this file.  Please review the following information to ensure the GNU General Public License version 3.0 requirements will be met: http://www.gnu.org/copyleft/gpl.html.
+
+If you are unsure which license is appropriate for your use, please contact the sales department at http://www.sencha.com/contact.
+
+*/
+/*!
+ * Ext JS Library 4.0
+ * Copyright(c) 2006-2011 Sencha Inc.
+ * licensing@sencha.com
+ * http://www.sencha.com/license
+ */
+
+Ext.define('MyDesktop.App', {
+    extend: 'Ext.ux.desktop.App',
+
+    requires: [
+        'Ext.window.MessageBox',
+
+        'Ext.ux.desktop.ShortcutModel',
+
+        'MyDesktop.SystemStatus',
+        'MyDesktop.VideoWindow',
+        'MyDesktop.GridWindow',
+        'MyDesktop.TabWindow',
+        'MyDesktop.AccordionWindow',
+        'MyDesktop.Notepad',
+        'MyDesktop.BogusMenuModule',
+        'MyDesktop.BogusModule',
+
+//        'MyDesktop.Blockalanche',
+        'MyDesktop.Settings'
+    ],
+
+    init: function() {
+        // custom logic before getXYZ methods get called...
+
+        this.callParent();
+
+        // now ready...
+    },
+
+    getModules : function(){
+        return [
+            new MyDesktop.VideoWindow(),
+            //new MyDesktop.Blockalanche(),
+            new MyDesktop.SystemStatus(),
+            new MyDesktop.GridWindow(),
+            new MyDesktop.TabWindow(),
+            new MyDesktop.AccordionWindow(),
+            new MyDesktop.Notepad(),
+            new MyDesktop.BogusMenuModule(),
+            new MyDesktop.BogusModule()
+        ];
+    },
+
+    getDesktopConfig: function () {
+        var me = this, ret = me.callParent();
+
+        return Ext.apply(ret, {
+            //cls: 'ux-desktop-black',
+
+            contextMenuItems: [
+                { text: 'Change Settings', handler: me.onSettings, scope: me }
+            ],
+
+            shortcuts: Ext.create('Ext.data.Store', {
+                model: 'Ext.ux.desktop.ShortcutModel',
+                data: [
+                    { name: '帐号设置', iconCls: 'grid-shortcut', module: 'grid-win' },
+                    { name: '查看作业', iconCls: 'accordion-shortcut', module: 'acc-win' },
+                    { name: '查看成绩', iconCls: 'notepad-shortcut', module: 'notepad' },
+                    { name: '学习资料', iconCls: 'cpu-shortcut', module: 'systemstatus'}
+                ]
+            }),
+
+            wallpaper: 'wallpapers/Blue-Sencha.jpg',
+            wallpaperStretch: false
+        });
+    },
+
+    // config for the start menu
+    getStartConfig : function() {
+        var me = this, ret = me.callParent();
+
+        return Ext.apply(ret, {
+            title: Ext.get('username')?Ext.get('username').dom.innerHTML:'GUEST',
+            iconCls: 'user',
+            height: 300,
+            toolConfig: {
+                width: 100,
+                items: [
+                    {
+                        text:'系统设置',
+                        iconCls:'settings',
+                        handler: me.onSettings,
+                        scope: me
+                    },
+                    '-',
+                    {
+                        text:'登录',
+                        iconCls:'logout',
+                        handler: me.onLogout,
+                        scope: me
+                    }
+                ]
+            }
+        });
+    },
+
+    getTaskbarConfig: function () {
+        var ret = this.callParent();
+
+        return Ext.apply(ret, {
+            quickStart: [
+                { name: 'Accordion Window', iconCls: 'accordion', module: 'acc-win' },
+                { name: 'Grid Window', iconCls: 'icon-grid', module: 'grid-win' }
+            ],
+            trayItems: [
+                { xtype: 'trayclock', flex: 1 }
+            ]
+        });
+    },
+
+    onLogout: function () {
+        Ext.Msg.confirm('Logout', 'Are you sure you want to logout?');
+    },
+
+    onSettings: function () {
+        var dlg = new MyDesktop.Settings({
+            desktop: this.desktop
+        });
+        dlg.show();
+    }
+});
+
